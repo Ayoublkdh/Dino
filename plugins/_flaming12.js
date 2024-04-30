6966d999bc47813761df8acbc3266847c681d27b
Ayoublkdh <162133258+Ayoublkdh@users.noreply.github.com>
2024-04-30 12:47:21 +0100

    Update _flaming12.js

--- plugins/_flaming12.js
+++ plugins/_flaming12.js
@@ -1,10 +1,10 @@
 import  fetch from 'node-fetch'
 let handler = async (m, { conn, args }) => {
   let response = args.join(' ').split('|')
-  if (!args[0]) throw '*_أكتب نص مع هاذا الأمر لإنشاء لوغو_*\n\n *مثال الإستخدام*\n .carved-wood JITOSSA'
+  if (!args[0]) throw '*_أكتب نص مع هاذا الأمر لإنشاء لوغو_*\n\n *مثال الإستخدام*\n .carved-wood AYOUB'
   m.reply('*_جارى إنشاء لوغو لك قريبا ..._*')
   let res = `https://api.tiodevhost.my.id/api/photooxy/carved-wood?text=${response[0]}`
-  conn.sendFile(m.chat, res, 'carved-wood.jpg', `> _JITOSSA LOGO_ \n\n www.instagram.com/ovmar_1`, m, false)
+  conn.sendFile(m.chat, res, 'carved-wood.jpg', `> _LEADER-DINO LOGO_ \n\n wa.me/212690943590`, m, false)
 }
 handler.help = ['carved-wood'].map(v => v + ' <text>')
 handler.tags = ['maker'']
