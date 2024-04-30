c026a40a256ceb1374176058b2db72b7618c35f6
Ayoublkdh <162133258+Ayoublkdh@users.noreply.github.com>
2024-04-30 12:46:20 +0100

    Update _flaming10.js

--- plugins/_flaming10.js
+++ plugins/_flaming10.js
@@ -1,7 +1,7 @@
 import  fetch from 'node-fetch'
 let handler = async (m, { conn, args }) => {
   let response = args.join(' ').split('|')
-  if (!args[0]) throw '*_أكتب نص مع هاذا الأمر لإنشاء لوغو_*\n\n *مثال الإستخدام*\n .logo-coffee JITOSSA'
+  if (!args[0]) throw '*_أكتب نص مع هاذا الأمر لإنشاء لوغو_*\n\n *مثال الإستخدام*\n .logo-coffee AYOUB'
   m.reply('*_جارى إنشاء لوغو لك قريبا ..._*')
   let res = `https://api.tiodevhost.my.id/api/photooxy/coffee-cup?text=${response[0]}`
   conn.sendFile(m.chat, res, 'coffee-cu.jpg', `> _LEADER-DINO LOGO_ \n\n wa.me/212690943590`, m, false)
