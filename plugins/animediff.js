11b6bc6f387cbf993b097de8c6f3c41d8b8ff0b8
Ayoublkdh <162133258+Ayoublkdh@users.noreply.github.com>
2024-04-30 13:56:36 +0100

    Update animediff.js

--- plugins/animediff.js
+++ plugins/animediff.js
@@ -3,13 +3,13 @@ import fetch from "node-fetch"
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
         let wm = global.me
-        if (!text) throw `*هاذا الأمر يقوم بتوليد صور أنمي*\n\nمثال للإستخدام\n${ usedPrefix + command } 1girl, blush, megane, school uniform`
+        if (!text) throw `*هاذا الأمر يقوم بتوليد صور أنمي*\n\nمثال للإستخدام\n${ usedPrefix + command } hd picture dinosaur with boy`
         await m.reply(waittt)
         await conn.relayMessage(m.chat, { reactionMessage: { key: m.key, text: '⌛'  }}, { messageId: m.key.id })
         try {
         let ff = await fetch(`https://api.neoxr.eu/api/waifudiff?q=${text}`)
         let anu = await ff.json()
-        await conn.sendFile(m.chat, anu.data.url, 'JITOSSA.jpg', '▢ *🥰 تابعني على إنستجرام*\n\n _*instagram.com/ovmar_1*_', m)
+        await conn.sendFile(m.chat, anu.data.url, 'LEADER.jpg', '▢ *🥰 للتواصل على واتساب*\n\n _*wa.me/212690943590*_', m)
         m.react('🎐')
       } catch (e) {
         console.log(e)
@@ -17,7 +17,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
       }
     }
 
-handler.help = ['animediff <text>']
+handler.help = ['animediff']
 handler.tags = ['drawing']
 handler.command = /^(animediff)$/i
 
