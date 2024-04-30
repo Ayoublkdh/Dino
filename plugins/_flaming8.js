ce52a56248602d00d55bb6893083346cd1b9d968
Ayoublkdh <162133258+Ayoublkdh@users.noreply.github.com>
2024-04-30 12:57:11 +0100

    Update _flaming8.js

--- plugins/_flaming8.js
+++ plugins/_flaming8.js
@@ -1,10 +1,10 @@
 import  fetch from 'node-fetch'
 let handler = async (m, { conn, args }) => {
   let response = args.join(' ').split('|')
-  if (!args[0]) throw '*_أكتب نص مع هاذا الأمر لإنشاء لوغو_*\n\n *مثال الإستخدام*\n .shadowsky JITOSSA'
+  if (!args[0]) throw '*_أكتب نص مع هاذا الأمر لإنشاء لوغو_*\n\n *مثال الإستخدام*\n .shadowsky AYOUB'
   m.reply('*_جارى إنشاء لوغو لك قريبا ..._*')
    let res = `https://api.tiodevhost.my.id/api/photooxy/shadow-sky?text=${response[0]}`
-  conn.sendFile(m.chat, res, 'carved.jpg', `> _JITOSSA LOGO_ \n\n www.instagram.com/ovmar_1`, m, false)
+  conn.sendFile(m.chat, res, 'carved.jpg', `> _LEADER LOGO_ \n\n wa.me/212690943590`, m, false)
 }
 handler.help = ['shadowsky'].map(v => v + ' <text>')
 handler.tags = ['maker'']
