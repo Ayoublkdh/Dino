5864d0d10bb918549ea8e6dc7677e8232cb84065
Ayoublkdh <162133258+Ayoublkdh@users.noreply.github.com>
2024-04-30 12:57:55 +0100

    Update _flaming9.js

--- plugins/_flaming9.js
+++ plugins/_flaming9.js
@@ -1,10 +1,10 @@
 import  fetch from 'node-fetch'
 let handler = async (m, { conn, args }) => {
   let response = args.join(' ').split('|')
-  if (!args[0]) throw '*_أكتب نص مع هاذا الأمر لإنشاء لوغو_*\n\n *مثال الإستخدام*\n .pictlove JITOSSA'
+  if (!args[0]) throw '*_أكتب نص مع هاذا الأمر لإنشاء لوغو_*\n\n *مثال الإستخدام*\n .pictlove AYOUB'
   m.reply('*_جارى إنشاء لوغو لك قريبا ..._*')
   let res = `https://api.tiodevhost.my.id/api/photooxy/picture-of-love?text=${response[0]}`
-  conn.sendFile(m.chat, res, 'pictlove.jpg', `> _JITOSSA LOGO_ \n\n www.instagram.com/ovmar_1`, m, false)
+  conn.sendFile(m.chat, res, 'pictlove.jpg', `> _LEADER LOGO_ \n\n wa.me/212690943590`, m, false)
 }
 handler.help = ['pictlove'].map(v => v + ' <text>')
 handler.tags = ['maker']
