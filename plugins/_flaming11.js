800e18e2d98de379abac11d1cbf5fd124ed6b757
Ayoublkdh <162133258+Ayoublkdh@users.noreply.github.com>
2024-04-30 12:45:20 +0100

    Update _flaming11.js

--- plugins/_flaming11.js
+++ plugins/_flaming11.js
@@ -1,10 +1,10 @@
 import  fetch from 'node-fetch'
 let handler = async (m, { conn, args }) => {
   let response = args.join(' ').split('|')
-  if (!args[0]) throw '*_أكتب نص مع هاذا الأمر لإنشاء لوغو_*\n\n *مثال الإستخدام*\n .butterfly JITOSSA'
+  if (!args[0]) throw '*_أكتب نص مع هاذا الأمر لإنشاء لوغو_*\n\n *مثال الإستخدام*\n .butterfly LEADER'
   m.reply('*_جارى إنشاء لوغو لك قريبا ..._*')
   let res = `https://api.tiodevhost.my.id/api/photooxy/butterfly?text=${response[0]}`
-  conn.sendFile(m.chat, res, 'butterfly.jpg', `> _JITOSSA LOGO_ \n\n www.instagram.com/ovmar_1`, m, false)
+  conn.sendFile(m.chat, res, 'butterfly.jpg', `> _LEADER-DINO LOGO_ \n\n wa.me/212690943590`, m, false)
 }
 handler.help = ['butterfly'].map(v => v + ' <text>')
 handler.tags = ['maker']
