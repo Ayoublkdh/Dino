61b5e6ef7a66ba6bbebd3359b4fdc574778969ea
Ayoublkdh <162133258+Ayoublkdh@users.noreply.github.com>
2024-05-01 08:16:45 +0100

    Update تخمين العلم.js

--- plugins/تخمين العلم.js
+++ plugins/تخمين العلم.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
 conn.reply(m.chat,`**\n\n*“${pickRandom(global.verdaad)}”*\n\n**`, m)
 }
-handler.help = ['ايمو']
+handler.help = ['كايمو']
 handler.tags = ['fun']
-handler.command = /^ايمو/i
+handler.command = /^كايمو/i
 export default handler
 function pickRandom(list) {
 return list[Math.floor(list.length * Math.random())]
