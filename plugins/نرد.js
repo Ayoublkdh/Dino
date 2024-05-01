ee2b079e4efda0c0442b647e51aa903d8ee241fd
Ayoublkdh <162133258+Ayoublkdh@users.noreply.github.com>
2024-05-01 08:30:07 +0100

    Update نرد.js

--- plugins/نرد.js
+++ plugins/نرد.js
@@ -9,7 +9,7 @@ const dir = [
 let handler = async (m, { conn }) => {
   conn.sendFile(m.chat, dir[Math.floor(Math.random() * dir.length)], 'dado.webp', '', m)
 }
-handler.help = ['dado']
+handler.help = ['نرد']
 handler.tags = ['game']
 handler.command = ['dado', 'dados','نرد'] 
 
