ced2eeff59f6242c34f46ec93b928860938b5c13
Ayoublkdh <162133258+Ayoublkdh@users.noreply.github.com>
2024-05-18 12:28:43 +0100

    Delete plugins/BOUTON.js

--- plugins/BOUTON.js
+++
@@ -1,59 +0,0 @@
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    conn.relayMessage(m.chat, {
-      viewOnceMessage: {
-        message: {
-          interactiveMessage: {
-            header: {
-              title: '* قـائـمـة الـاوامــر الديناصورة 🦖*'
-            },
-            body: {
-              text: '🛡️ افتح القائمة بواسطة الزر\n⚡ لا تلعب كثير في القائمة'
-            },
-            nativeFlowMessage: {
-              buttons: [
-                {
-                  name: 'single_select',
-                  buttonParamsJson: JSON.stringify({
-                    title: 'دوس هنا ',
-                    sections: [
-                      {
-                        title: 'قوائم البوت',
-                        highlight_label: 'اختار',
-                        rows: [
-                          {
-                            header: 'قسم الانمي',
-                            title: '.الانمي',
-                            description: '',
-                            id: 'te'
-                          },
-                          {
-                            header: 'قسم الاوامر',
-                            title: '.المهام',
-                            description: '',
-                            id: 'te'
-                          }
-                        ]
-                      }
-                    ]
-                  }),
-                  messageParamsJson: ''
-                }
-              ]
-            }
-          }
-        }
-      }
-    }, {})
-
-}
-
-handler.help = ['info']
-handler.tags = ['main']
-handler.command = ['القائمة']
-
-export default handler
-
-
-`ملحوظة القائمة لي حصريا` 
-
-> ROPERTO🇪🇬
