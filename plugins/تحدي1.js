554e64088c45616282687eb4b352b447315c033e
Ayoublkdh <162133258+Ayoublkdh@users.noreply.github.com>
2024-05-19 09:20:41 +0100

    Update تحدي1.js

--- plugins/تحدي1.js
+++ plugins/تحدي1.js
@@ -9,7 +9,7 @@ const handler = async (m, {conn, usedPrefix, text}) => {
   if (!m.mentionedJid[0]) return m.reply(textquien, m.chat, {mentions: conn.parseMention(textquien)});
   if (Object.values(conn.suit).find((room) => room.id.startsWith('suit') && [room.p, room.p2].includes(m.mentionedJid[0]))) throw `*[❗] 𝙻𝙰 𝙿𝙴𝚁𝚂𝙾𝙽𝙰 𝙰 𝙻𝙰 𝚀𝚄𝙴 𝚀𝚄𝙸𝙴𝚁𝙴𝚂 𝙳𝙴𝚂𝙰𝙵𝙸𝙰𝚁 𝙰𝚄𝙽 𝙴𝚂𝚃𝙰 𝙹𝚄𝙶𝙰𝙽𝙳𝙾 𝙾𝚃𝚁𝙰 𝙿𝙰𝚁𝚃𝙸𝙳𝙰, 𝙴𝚂𝙿𝙴𝚁𝙰 𝙰 𝚀𝚄𝙴 𝚃𝙴𝚁𝙼𝙸𝙽𝙴 𝙳𝙴 𝙹𝚄𝙶𝙰𝚁`;
   const id = 'suit_' + new Date() * 1;
-  const caption = `🎮 *لعبة حجر ورقه مقص* 🎮\n\n—◉ @${m.sender.split`@`[0]} شايـف نفـسو وبيتحـداك تعـال علـم عليه🦦 @${m.mentionedJid[0].split`@`[0]} موافـق عل التحـدي\n◉ اكتب "نعم" للموافقه\n◉ اكتب "لا" للرفض\n`;
+  const caption = `🎮 *لعبة حجر ورقه مقص* 🎮\n\n—◉ @${m.sender.split`@`[0]} شايـف نفـسو وبيتحـداك تعـال علـم عليه🦦 @${m.mentionedJid[0].split`@`[0]} موافـق عل التحـدي\n◉ اكتب "accept" للموافقه\n◉ اكتب "gamau" للرفض\n`;
   const imgplaygame = `https://www.merca2.es/wp-content/uploads/2020/05/Piedra-papel-o-tijera-0003318_1584-825x259.jpeg`;
   conn.suit[id] = {
     chat: await conn.sendMessage(m.chat, {text: caption}, {mentions: await conn.parseMention(caption)}),
